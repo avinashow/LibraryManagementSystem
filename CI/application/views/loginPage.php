cb863ee153c26a73a2002cc78bd04f80d55ab9df
avinashow <avinashow@gmail.com>
2016-03-15 02:21:51 -0400

    check the new login page bro
    masthu undi chudu

--- CI/application/views/loginPage.php
+++ CI/application/views/loginPage.php
@@ -7,23 +7,56 @@
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
+		<link rel="stylesheet" href="assets/css/loginPage.css"/>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-		<script type="text/javascript">
+		<script>
+			$(document).ready(function(){
+			  $(document).mousemove(function(e){
+			     TweenLite.to($('body'), 
+			        .5, 
+			        { css: 
+			            {
+			                backgroundPosition: ""+ parseInt(event.pageX/8) + "px "+parseInt(event.pageY/'12')+"px, "+parseInt(event.pageX/'15')+"px "+parseInt(event.pageY/'15')+"px, "+parseInt(event.pageX/'30')+"px "+parseInt(event.pageY/'30')+"px"
+			            }
+			        });
+			  });
+			});
 		</script>
+		<script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
+
 	</head>
 
 	<body>
 		<div class="container">
-			<form action="Login/validate" method="post">
-				<input type="text" name="uname" id="uname"/>
-				<input type="submit" id="submit" value="submit"/>
-			</form>
-			<div id="msg">
-				<?php if ($msg == "" ) { } else {echo $msg;} ?>
+			<div class="row vertical-offset-100">
+		    	<div class="col-md-4 col-md-offset-4">
+		    		<div class="panel panel-default">
+					  	<div class="panel-heading">
+					    	<h3 class="panel-title">Please sign in</h3>
+					 	</div>
+					  	<div class="panel-body">
+					    	<form accept-charset="UTF-8" role="form">
+		                    <fieldset>
+					    	  	<div class="form-group">
+					    		    <input class="form-control" placeholder="E-mail" name="email" type="text">
+					    		</div>
+					    		<div class="form-group">
+					    			<input class="form-control" placeholder="Password" name="password" type="password" value="">
+					    		</div>
+					    		<div class="checkbox">
+					    	    	<label>
+					    	    		<input name="remember" type="checkbox" value="Remember Me"> Remember Me
+					    	    	</label>
+					    	    </div>
+					    		<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+					    	</fieldset>
+					      	</form>
+					    </div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
