14d2e18fa105c3ecfd5f4be676477f441b0e3317
avinashow <avinashow@gmail.com>
2016-04-20 00:57:21 -0400

    light changes to the form

--- CI/application/views/homePage.php
+++ CI/application/views/homePage.php
@@ -121,7 +121,7 @@
 		  <!-- Collect the nav links, forms, and other content for toggling -->
 		  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		    <ul class="nav navbar-nav" id="links">
-		      <li class="active"><a href="#">Home</a></li>
+		      <li><a href="search">Home</a></li>
 		    </ul>
 		    <div class="col-sm-3 col-md-3">
 		        <form class="navbar-form" role="search">
--- CI/application/views/myaccount.php
+++ CI/application/views/myaccount.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 
-		<title>Login Page</title>
+		<title>MyAccount Page</title>
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -19,7 +19,7 @@
 		<script>	
 			$(document).ready(function() {
 				$("#id").text("welcome " + sessionStorage.getItem("username"));
-
+				$("#search").hide();
 				$("#logout").click(function(){
 					sessionStorage.removeItem("username");
 					window.location="search";
